1d17a3a3724ca69e2559fabca13d2589bdf85c14
Vitaliy <vitalik.vinnik.05@gmail.com>
2023-03-29 11:52:33 +0300

    add cameras

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -1,3 +1,4 @@
+using Core.Enums;
 using UnityEngine;
 
 namespace Player
@@ -7,10 +8,12 @@ namespace Player
     {
         [Header("HorizontalMovement")]
         [SerializeField] private float _horizontalSpeed;
-        [SerializeField] private bool _faceRight;
+        [SerializeField] private Direction _direction;
 
         [Header("Jump")]
         [SerializeField] private float _jumpForce;
+
+        [SerializeField] private DirectionalCameraPair _cameras;
 
         private Rigidbody2D _rigidbody;
         
@@ -62,15 +65,17 @@ namespace Player
         
         private void SetDirection(float direction)
         {
-            if(_faceRight && direction < 0 || 
-               !_faceRight && direction > 0)
+            if(_direction == Direction.Right && direction < 0 || 
+               _direction == Direction.Left && direction > 0)
                 Flip();
         }
 
         private void Flip()
         {
             transform.Rotate(0, 180, 0);
-            _faceRight = !_faceRight;
+            _direction = _direction == Direction.Right ? Direction.Left : Direction.Right;
+            foreach (var cameraPair in _cameras.DirectionalCameras)
+                cameraPair.Value.enabled = cameraPair.Key == _direction;
         }
     }
 }
