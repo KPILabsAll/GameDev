922439551d0b90abba004d0fda0fb5dbf5d0e444
Vitaliy <vitalik.vinnik.05@gmail.com>
2023-03-29 16:52:51 +0300

    add ui buttons

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Assets.Scripts.Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        [SerializeField] private GameUIInputView _gameUIInputView;
+
+        private ExternalDevicesInputReader _externalDevicesInputReader;
+        private PlayerBrain _playerBrain;
+
+        private bool _onPause;
+        private void Awake()
+        {
+            _externalDevicesInputReader = new();
+            _playerBrain = new(_playerEntity, new()
+            {
+                _gameUIInputView,
+                _externalDevicesInputReader
+            });
+        }
+        private void Update()
+        {
+            if (_onPause)
+                return;
+            _externalDevicesInputReader.OnUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_onPause)
+                return;
+            _playerBrain.OnFixedUpdate();
+        }
+    }
+}
